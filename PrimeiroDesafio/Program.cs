5f296ac5fd17e37c0be3c3ae7140fed26c40a23a
Mari Afonso <marianaafonso007@gmail.com>
2023-11-20 20:32:16 -0300

    Evoluindo o código

--- PrimeiroDesafio/Program.cs
+++ PrimeiroDesafio/Program.cs
@@ -9,11 +9,11 @@ Console.WriteLine("Digite a idade da Segunda pessoa: ");
 var idade2 = Convert.ToInt32(Console.ReadLine());
 
 var maiorIdade1 = idade1 > idade2;
-var maiorDeIdade1 = idade1 > 17;
-var maiorDeIdade2 = idade2 > 17;
+var maiorDeIdade1 = VerificaMaiorIdade(idade1);
+var maiorDeIdade2 = VerificaMaiorIdade(idade2);
 
 Console.WriteLine("A primeira pessoa é mais velho que a segunda?");
-Console.WriteLine(maiorIdade1);
+Console.WriteLine(maiorDeIdade1);
 
 Console.WriteLine("A primeira pessoa é maior que 18 anos?");
 Console.WriteLine(maiorDeIdade1);
@@ -22,5 +22,9 @@ Console.WriteLine("A segunda pessoa é maior que 18 anos?");
 Console.WriteLine(maiorDeIdade2);
 
 
-
+bool VerificaMaiorIdade(int idade)
+{
+    var maiorDeIdade = idade >= 18;
+    return maiorDeIdade;
+}
 
